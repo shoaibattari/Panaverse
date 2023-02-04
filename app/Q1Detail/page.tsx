9b466e41f5dfd077b21225764390112f3733345c
shoaib memon <shoaibattari83@gmail.com>
2023-02-05 01:33:14 +0500

    q1 detail updated

--- app/Q1Detail/page.tsx
+++ app/Q1Detail/page.tsx
@@ -1,9 +1,243 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import NextLink from "next/link";
+
+import { Box, Heading, LinkBox, Text } from "@chakra-ui/react";
 
 function Q1Detail() {
   return (
-    <div>Q1Detail</div>
-  )
+    <>
+      <Box bg="linear-gradient(120deg, #155799, #159957)" p={"70px"}>
+        <Text
+          fontWeight="bold"
+          fontSize="5xl"
+          align="center"
+          color="white"
+          m="10px"
+        >
+          CS-101: Object-Oriented Programming using TypeScript
+          <br />
+          Duration: 13 Weeks
+          <br />
+          Course Description:
+        </Text>
+      </Box>
+      <Box boxSize="3xl" ml="25%" p="5">
+        <Text fontSize="2xl" textColor="green.500" p="3">
+          We will start the program by learning the fundamentals of
+          Object-Oriented programming using JavaScript and TypeScript. We will
+          also understand the latest Web trends i.e. Web 3.0 and Metaverse
+          concepts and try to understand their working from the perspective of
+          the users.
+        </Text>
+        <Text fontSize="2xl" textColor="blue.500" p="3">
+          Course Outline:
+        </Text>
+        <Text fontSize="2xl" align="center" color="green.500">
+          HTML and CSS (Homework)
+        </Text>
+        <LinkBox
+          as="article"
+          maxW="3xl"
+          p="3"
+          textColor="gray.200"
+          bg="linear-gradient(120deg, #155799, #159957)"
+          borderRadius="20px"
+        >
+          <Heading size="md" my="3">
+            <NextLink href="https://www.youtube.com/playlist?list=PLKvqnz8z1zWQ3BALy86tIXICkG874wAc6">
+              Learn HTML by Hira Khan (Watch Recorded Videos)
+            </NextLink>
+          </Heading>
+        </LinkBox>
+        <br />
+        <LinkBox
+          as="article"
+          maxW="3xl"
+          p="3"
+          textColor="gray.200"
+          borderWidth="1px"
+          rounded="md"
+          bg="linear-gradient(120deg, #155799, #159957)"
+          borderRadius="20px"
+        >
+          <Heading size="md" my="3">
+            <NextLink href="https://www.youtube.com/playlist?list=PLKvqnz8z1zWQSWIen_zUSEBmtqzPLuRob">
+              Learn CSS Intro by Hira Khan (Watch Recorded Videos)
+            </NextLink>
+          </Heading>
+        </LinkBox>
+        <Text fontSize="2xl" align="center" color="green.500">
+          Web 3.0 and Metaverse Theory
+        </Text>
+        <br />
+        <LinkBox
+          as="article"
+          maxW="3xl"
+          p="3"
+          textColor="gray.200"
+          borderWidth="1px"
+          rounded="md"
+          bg="linear-gradient(120deg, #155799, #159957)"
+          borderRadius="20px"
+        >
+          <Heading size="md" my="3">
+            <NextLink href="https://docs.google.com/presentation/d/12C1s4UBTlR9nZNEkRE6aAuGeRA3s92jx-8nJKo9jdH4/edit">
+              Introduction to Panaverse DAO
+            </NextLink>
+          </Heading>
+        </LinkBox>
+        <br />
+        <LinkBox
+          as="article"
+          maxW="3xl"
+          p="3"
+          textColor="gray.200"
+          borderWidth="1px"
+          rounded="md"
+          bg="linear-gradient(120deg, #155799, #159957)"
+          borderRadius="20px"
+        >
+          <Heading size="md" my="3">
+            <NextLink href="https://docs.google.com/presentation/d/1FSbr9aJwO0-fmZHqWy_eHO2N_jwJLmQCy4cG8rd4ctw/edit">
+              Web 3.0 User Guide
+            </NextLink>
+          </Heading>
+        </LinkBox>
+        <Text fontSize="2xl" align="center" color="green.500">
+          Complete Web 3 Assignments included in the Web 3 User Guide
+        </Text>
+        <br />
+        <LinkBox
+          as="article"
+          maxW="3xl"
+          p="3"
+          textColor="gray.200"
+          borderWidth="1px"
+          rounded="md"
+          bg="linear-gradient(120deg, #155799, #159957)"
+          borderRadius="20px"
+        >
+          <Heading size="md" my="3">
+            <NextLink href="https://docs.google.com/presentation/d/1ADk87hQ0Etr2PfmN9XH7TQ0CHl6XSP_7JWNUbzPdDNc/edit">
+              Virtual and Augmented Metaverse User Guide
+            </NextLink>
+          </Heading>
+        </LinkBox>
+        <Text fontSize="2xl" align="center" color="green.500">
+          Fundamentals of JavaScript
+        </Text>
+        <br />
+        <LinkBox
+          as="article"
+          maxW="3xl"
+          p="3"
+          textColor="gray.200"
+          borderWidth="1px"
+          rounded="md"
+          bg="linear-gradient(120deg, #155799, #159957)"
+          borderRadius="20px"
+        >
+          <Heading size="md" my="3">
+            <NextLink href="https://www.youtube.com/playlist?list=PLKvqnz8z1zWQdc0NSLknxmxBch5gLOqyo">
+              Learn JavaScript by Zeeshan Hanif (Watch Recorded Videos)
+            </NextLink>
+          </Heading>
+        </LinkBox>
+        <br />
+        <LinkBox
+          as="article"
+          maxW="3xl"
+          p="3"
+          textColor="gray.200"
+          borderWidth="1px"
+          rounded="md"
+          bg="linear-gradient(120deg, #155799, #159957)"
+          borderRadius="20px"
+        >
+          <Heading size="md" my="3">
+            <NextLink href="https://github.com/panaverse/typescript-node-projects/blob/main/getting-started-exercises.md">
+              Getting Started Exercises with JavaScript and Node.js
+            </NextLink>
+          </Heading>
+        </LinkBox>
+        <br />{" "}
+        <Text fontSize="2xl" align="center" color="green.500">
+          Object-Oriented Programming with TypeScript
+        </Text>
+        <br />
+        <LinkBox
+          as="article"
+          maxW="3xl"
+          p="3"
+          textColor="gray.200"
+          borderWidth="1px"
+          rounded="md"
+          bg="linear-gradient(120deg, #155799, #159957)"
+          borderRadius="20px"
+        >
+          <Heading size="md" my="3">
+            <NextLink href="https://github.com/panaverse/learn-typescript">
+              Learning Repository TypeScript
+            </NextLink>
+          </Heading>
+        </LinkBox>
+        <br />
+        <LinkBox
+          as="article"
+          maxW="3xl"
+          p="3"
+          textColor="gray.200"
+          borderWidth="1px"
+          rounded="md"
+          bg="linear-gradient(120deg, #155799, #159957)"
+          borderRadius="20px"
+        >
+          <Heading size="md" my="3">
+            <NextLink href="https://github.com/panaverse/typescript-node-projects">
+              Homework Project TypeScript
+            </NextLink>
+          </Heading>
+        </LinkBox>
+        <br />
+        <LinkBox
+          as="article"
+          maxW="3xl"
+          p="3"
+          textColor="gray.200"
+          borderWidth="1px"
+          rounded="md"
+          bg="linear-gradient(120deg, #155799, #159957)"
+          borderRadius="20px"
+        >
+          <Heading size="md" my="3">
+            <NextLink href="https://github.com/panaverse/typescript-node-projects">
+              Homework Project TypeScript
+            </NextLink>
+          </Heading>
+        </LinkBox>
+        <br />
+        <LinkBox
+          as="article"
+          maxW="3xl"
+          p="3"
+          textColor="gray.200"
+          borderWidth="1px"
+          rounded="md"
+          bg="linear-gradient(120deg, #155799, #159957)"
+          borderRadius="20px"
+        >
+          <Heading size="md" my="3">
+            <NextLink href="https://profy.dev/article/react-typescript">
+              TypeScript for React
+            </NextLink>
+          </Heading>
+        </LinkBox>
+        <br /><br /><br />
+      </Box>
+    </>
+  );
 }
 
-export default Q1Detail
+export default Q1Detail;
