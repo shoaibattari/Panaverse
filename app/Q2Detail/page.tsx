fe29228858097181a226f8ee1405726e1a83e88f
shoaib memon <shoaibattari83@gmail.com>
2023-02-05 11:34:39 +0500

    quarter 2 complete

--- app/Q2Detail/page.tsx
+++ app/Q2Detail/page.tsx
@@ -41,7 +41,8 @@ function Q2Detail() {
           Course Outline:
         </Text>
         <Text fontSize="2xl" align="center" color="green.500">
-        Next.js 13 Web Development        </Text>
+          Next.js 13 Web Development{" "}
+        </Text>
         <LinkBox
           as="article"
           maxW="3xl"
@@ -52,7 +53,7 @@ function Q2Detail() {
         >
           <Heading size="md" my="3">
             <Link href="https://beta.nextjs.org/docs">
-            Next.js 13 Web Development
+              Next.js 13 Web Development
             </Link>
           </Heading>
         </LinkBox>
@@ -67,9 +68,11 @@ function Q2Detail() {
         >
           <Heading size="md" my="3">
             <Link href="https://beta.reactjs.org/learn">
-            Latest Learn React Official Website            </Link>
+              Latest Learn React Official Website{" "}
+            </Link>
           </Heading>
-        </LinkBox><br />
+        </LinkBox>
+        <br />
         <LinkBox
           as="article"
           maxW="3xl"
@@ -80,14 +83,341 @@ function Q2Detail() {
         >
           <Heading size="md" my="3">
             <Link href="https://github.com/panaverse/learn-nextjs">
-            Learn Next.js 13 Learning Repo          </Link>
+              Learn Next.js 13 Learning Repo{" "}
+            </Link>
           </Heading>
         </LinkBox>
-
-
-
-
-
+        <br />
+        <Text fontSize="2xl" align="center" color="green.500">
+          Next.js 13 using Chakra UI (Remote Zoom Class)
+        </Text>
+        <LinkBox
+          as="article"
+          maxW="3xl"
+          p="3"
+          textColor="gray.200"
+          bg="linear-gradient(120deg, #155799, #159957)"
+          borderRadius="20px"
+        >
+          <Heading size="md" my="3">
+            <Link href="https://chakra-ui.com/getting-started">
+              Chakra UI Docs
+            </Link>
+          </Heading>
+        </LinkBox>
+        <br />
+        <LinkBox
+          as="article"
+          maxW="3xl"
+          p="3"
+          textColor="gray.200"
+          bg="linear-gradient(120deg, #155799, #159957)"
+          borderRadius="20px"
+        >
+          <Heading size="md" my="3">
+            <Link href="https://www.freecodecamp.org/news/css-flexbox-complete-guide/">
+              CSS Flexbox Explained – Complete Guide to Flexible Containers and
+              Flex Items
+            </Link>
+          </Heading>
+        </LinkBox>
+        <br />
+        <Text fontSize="2xl" align="center" color="green.500">
+          UI/UX Design with Figma, TailwindCSS, and Chakra UI (Remote Zoom
+          Class)
+        </Text>
+        <LinkBox
+          as="article"
+          maxW="3xl"
+          p="3"
+          textColor="gray.200"
+          bg="linear-gradient(120deg, #155799, #159957)"
+          borderRadius="20px"
+        >
+          <Heading size="md" my="3">
+            <Link href="https://chakra-ui.com/getting-started">
+              Designing and Prototyping Interfaces with Figma: Learn essential
+              UX/UI design principles by creating interactive prototypes for
+              mobile, tablet, and desktop by Fabio Staiano
+            </Link>
+          </Heading>
+        </LinkBox>
+        <br />
+        <LinkBox
+          as="article"
+          maxW="3xl"
+          p="3"
+          textColor="gray.200"
+          bg="linear-gradient(120deg, #155799, #159957)"
+          borderRadius="20px"
+        >
+          <Heading size="md" my="3">
+            <Link href="https://www.figma.com/community/file/768809027799962739">
+              Figma Design Kit for TailwindCSS
+            </Link>
+          </Heading>
+        </LinkBox>
+        <br />
+        <LinkBox
+          as="article"
+          maxW="3xl"
+          p="3"
+          textColor="gray.200"
+          bg="linear-gradient(120deg, #155799, #159957)"
+          borderRadius="20px"
+        >
+          <Heading size="md" my="3">
+            <Link href="https://www.figma.com/community/file/971408767069651759">
+              Chakra UI Figma Kit
+            </Link>
+          </Heading>
+        </LinkBox>
+        <br />
+        <Text fontSize="2xl" align="center" color="green.500">
+          API Routes with Next.js
+        </Text>
+        <br />
+        <LinkBox
+          as="article"
+          maxW="3xl"
+          p="3"
+          textColor="gray.200"
+          bg="linear-gradient(120deg, #155799, #159957)"
+          borderRadius="20px"
+        >
+          <Heading size="md" my="3">
+            <Link href="https://nextjs.org/docs/api-routes/introduction">
+              API Routes
+            </Link>
+          </Heading>
+        </LinkBox>
+        <br />
+        <LinkBox
+          as="article"
+          maxW="3xl"
+          p="3"
+          textColor="gray.200"
+          bg="linear-gradient(120deg, #155799, #159957)"
+          borderRadius="20px"
+        >
+          <Heading size="md" my="3">
+            <Link href="https://codevoweb.com/build-a-trpc-crud-api-example-with-next-js/">
+              Build a tRPC CRUD API Example with Next.js
+            </Link>
+          </Heading>
+        </LinkBox>
+        <br />{" "}
+        <LinkBox
+          as="article"
+          maxW="3xl"
+          p="3"
+          textColor="gray.200"
+          bg="linear-gradient(120deg, #155799, #159957)"
+          borderRadius="20px"
+        >
+          <Heading size="md" my="3">
+            <Link href="https://brockherion.dev/blog/posts/stop-building-rest-apis-for-your-next-apps/">
+              Stop building REST APIs for your Next.js apps, use tRPC instead
+            </Link>
+          </Heading>
+        </LinkBox>
+        <br />
+        <Text fontSize="2xl" align="center" color="green.500">
+          SQL and Prisma
+        </Text>
+        <br />
+        <LinkBox
+          as="article"
+          maxW="3xl"
+          p="3"
+          textColor="gray.200"
+          bg="linear-gradient(120deg, #155799, #159957)"
+          borderRadius="20px"
+        >
+          <Heading size="md" my="3">
+            <Link href="https://www.prisma.io/docs/getting-started/setup-prisma/start-from-scratch/relational-databases-typescript-postgres">
+              Start from scratch with relational databases
+            </Link>
+          </Heading>
+        </LinkBox>
+        <br />
+        <LinkBox
+          as="article"
+          maxW="3xl"
+          p="3"
+          textColor="gray.200"
+          bg="linear-gradient(120deg, #155799, #159957)"
+          borderRadius="20px"
+        >
+          <Heading size="md" my="3">
+            <Link href="https://www.youtube.com/watch?v=5hzZtqCNQKk">
+              SQL For Beginners Video Tutorial
+            </Link>
+          </Heading>
+        </LinkBox>
+        <br />
+        <LinkBox
+          as="article"
+          maxW="3xl"
+          p="3"
+          textColor="gray.200"
+          bg="linear-gradient(120deg, #155799, #159957)"
+          borderRadius="20px"
+        >
+          <Heading size="md" my="3">
+            <Link href="https://www.freecodecamp.org/news/dbms-and-sql-basics/">
+              Database Management Systems and SQL – Tutorial for Beginners
+            </Link>
+          </Heading>
+        </LinkBox>
+        <Text fontSize="2xl" align="center" color="green.500">
+          Next.js 13 using TailwindCSS (Remote Zoom Class)
+        </Text>
+        <br />
+        <LinkBox
+          as="article"
+          maxW="3xl"
+          p="3"
+          textColor="gray.200"
+          bg="linear-gradient(120deg, #155799, #159957)"
+          borderRadius="20px"
+        >
+          <Heading size="md" my="3">
+            <Link href="https://pragprog.com/titles/tailwind2/modern-css-with-tailwind-second-edition/">
+              Modern CSS with Tailwind, Second Edition by Noel Rappin{" "}
+            </Link>
+          </Heading>
+        </LinkBox>
+        <Text fontSize="2xl" align="center" color="green.500">
+          AWS Application Composer (Remote Zoom Class){" "}
+        </Text>
+        <LinkBox
+          as="article"
+          maxW="3xl"
+          p="3"
+          textColor="gray.200"
+          bg="linear-gradient(120deg, #155799, #159957)"
+          borderRadius="20px"
+        >
+          <Heading size="md" my="3">
+            <Link href="https://www.youtube.com/watch?v=BujE_tik5r8">
+              What is AWS Application Composer?
+            </Link>
+          </Heading>
+        </LinkBox>
+        <br />
+        <LinkBox
+          as="article"
+          maxW="3xl"
+          p="3"
+          textColor="gray.200"
+          bg="linear-gradient(120deg, #155799, #159957)"
+          borderRadius="20px"
+        >
+          <Heading size="md" my="3">
+            <Link href="https://www.youtube.com/watch?v=p411uh363jQ">
+              Event-driven apps with AWS Application Composer
+            </Link>
+          </Heading>
+        </LinkBox>
+        <br />
+        <LinkBox
+          as="article"
+          maxW="3xl"
+          p="3"
+          textColor="gray.200"
+          bg="linear-gradient(120deg, #155799, #159957)"
+          borderRadius="20px"
+        >
+          <Heading size="md" my="3">
+            <Link href="https://aws.amazon.com/application-composer/">
+              Visually design and build serverless applications quickly
+            </Link>
+          </Heading>
+        </LinkBox>
+        <Text fontSize="2xl" align="center" color="green.500">
+          Multi-Cloud GraphQL Serverless API Development with Cloud Development
+          Kit for Terraform (CDKTF) (Remote Zoom Class){" "}
+        </Text>
+        <br />
+        <LinkBox
+          as="article"
+          maxW="3xl"
+          p="3"
+          textColor="gray.200"
+          bg="linear-gradient(120deg, #155799, #159957)"
+          borderRadius="20px"
+        >
+          <Heading size="md" my="3">
+            <Link href="https://github.com/panaverse/learn-multicloud-api-development">
+              Learn Multicloud Serverless API Development using CDK Terraform in  
+              Baby Steps
+            </Link>
+          </Heading>
+        </LinkBox>
+        <Text fontSize="2xl" align="center" color="green.500">
+          Web 2.0 Projects{" "}
+        </Text>
+        <br />
+        <LinkBox
+          as="article"
+          maxW="3xl"
+          p="3"
+          textColor="gray.200"
+          bg="linear-gradient(120deg, #155799, #159957)"
+          borderRadius="20px"
+        >
+          <Heading size="md" my="3">
+            <Link href="https://github.com/panaverse/nextjs-projects">
+              Next.js Projects
+            </Link>
+          </Heading>
+        </LinkBox>
+        <br />
+        <LinkBox
+          as="article"
+          maxW="3xl"
+          p="3"
+          textColor="gray.200"
+          bg="linear-gradient(120deg, #155799, #159957)"
+          borderRadius="20px"
+        >
+          <Heading size="md" my="3">
+            <Link href="https://github.com/panaverse/styling-nextjs-projects">
+              Styling Next.js Projects using TailwindCSS and Chakra UI
+            </Link>
+          </Heading>
+        </LinkBox>
+        <br />
+        <LinkBox
+          as="article"
+          maxW="3xl"
+          p="3"
+          textColor="gray.200"
+          bg="linear-gradient(120deg, #155799, #159957)"
+          borderRadius="20px"
+        >
+          <Heading size="md" my="3">
+            <Link href="https://github.com/ogzhanolguncu/min-todo">
+              Todo Full-Stack App
+            </Link>
+          </Heading>
+        </LinkBox>
+        <br />
+        <LinkBox
+          as="article"
+          maxW="3xl"
+          p="3"
+          textColor="gray.200"
+          bg="linear-gradient(120deg, #155799, #159957)"
+          borderRadius="20px"
+        >
+          <Heading size="md" my="3">
+            <Link href="https://www.youtube.com/watch?v=nzJsYJPCc80">
+              Build a Twitter Clone
+            </Link>
+          </Heading>
+        </LinkBox>
       </Box>
     </>
   );
