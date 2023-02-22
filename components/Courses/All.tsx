4d52f708ef4001b6af048a2a8fd0d4fb4c134837
shoaib memon <shoaibattari83@gmail.com>
2023-02-22 11:14:20 +0500

    home page responsive

--- components/Courses/All.tsx
+++ components/Courses/All.tsx
@@ -21,7 +21,7 @@ function ALL() {
       boxSize={{ lg: "3xl", md: "2xl" }}
       ml={{ lg: "25%", base: "0%" }}
       p="5"
-      mt={{ lg: "24",md:'24', base: "1" }}
+      mt={{ lg: "24", md: "24", base: "1" }}
     >
       <Text
         bg="linear-gradient(120deg, #155799, #159957)"
@@ -35,7 +35,7 @@ function ALL() {
         Core Courses (Common in All Specializations)
       </Text>
       <Text
-        boxSize={{md:"3xl"}}
+        boxSize={{ md: "3xl" }}
         ml="auto"
         p="5"
         mt="2"
@@ -48,13 +48,13 @@ function ALL() {
       </Text>
 
       <SimpleGrid
-        mt={{lg:"-620",md:-620, base:"0"}}
+        mt={{ lg: "-620", md: "-620", base: "0" }}
         spacing={0.2}
         templateColumns="repeat(auto-fill, minmax(250px, 1fr))"
       >
         <Card m="2">
           <CardHeader>
-            <Heading textColor="green.600" size={{lg:"md", base:"S"}}>
+            <Heading textColor="green.600" size={{ lg: "md", base: "S" }}>
               Quarter I (Core) CS-101: Object-Oriented Programming using
               TypeScript
             </Heading>
@@ -77,7 +77,7 @@ function ALL() {
         </Card>
         <Card m="2">
           <CardHeader>
-          <Heading textColor="green.600" size={{lg:"md", base:"S"}}>
+            <Heading textColor="green.600" size={{ lg: "md", base: "S" }}>
               Quarter II (Core) W2-201: Developing Planet-Scale Web 2.0
               Serverless Cloud Cloud Apps and APIs using Next.js 13 and Cloud
               Development Kit (CDK) for Terraform
@@ -100,12 +100,16 @@ function ALL() {
         </Card>
       </SimpleGrid>
 
-      <Text  size={{lg:"md", base:"S"}}
-       align="left" textColor="green.600" mt="5">
+      <Text
+        size={{ lg: "md", base: "S" }}
+        align="left"
+        textColor="green.600"
+        mt="5"
+      >
         Specialized Tracks
       </Text>
       <Text
-        boxSize={{ lg: "3xl", md: "2xl" }}
+        boxSize={{ lg: "3xl", md: "1xl" }}
         ml="auto"
         p="5"
         mt="2"
@@ -117,172 +121,196 @@ function ALL() {
         or more specializations consisting of two courses each:
       </Text>
       <hr />
-      {/* //////////////////////////////////////////////////////////////////////////////////////////////////////////// */}
-      <Box 
-       boxSize={{ lg: "3xl", md: "2xl" }}
-      ml={{ base: "0%" }}
-      p="5"
-      mt={{ lg: "24",md:'24', base: "1" }}
+      {/* /////////// ///////////////////////////////////////////////////////////////////////////////////////////////// */}
+      <Box
+        boxSize={{ lg: "3xl", md: "2xl" }}
+        ml={{ base: "0%" }}
+        p="5"
+        mt={{ lg: "-960", base: "1" }}
       >
-          <Box as="div" mt={{lg:'96', base: '0'}}>
-            <Text
-              bg="linear-gradient(120deg, #155799, #159957)"
-              fontSize={{ lg: "3xl", md: "2xl", base: "sm" }}
-              align="center"
-              textColor="gray.200"
-              borderRadius="20"
-            >
-              Web 3.0 (Blockchain) and Metaverse Specialization
-            </Text>
-            <Text
-                   boxSize={{ lg: "3xl", md: "2xl" }}
+        <Box as="div" mt={{ lg: "96", base: "0" }}>
+          <Text
+            bg="linear-gradient(120deg, #155799, #159957)"
+            fontSize={{ lg: "3xl", md: "2xl", base: "sm" }}
+            align="center"
+            textColor="gray.200"
+            borderRadius="20"
+          >
+            Web 3.0 (Blockchain) and Metaverse Specialization
+          </Text>
+          <Text
+            boxSize={{ lg: "3xl", md: "2xl" }}
+            ml="auto"
+            p="5"
+            mt={{ lg: "10" }}
+            fontSize={{ lg: "3xl", md: "2xl", base: "sm" }}
+            align="left"
+            textColor="gray.600"
+          >
+            This Web 3.0 and Metaverse specialization focuses on developing
+            full-stack Web 3.0 and Metaverse experiences for the next generation
+            of the internet by specializing in building worlds that merge the
+            best of cutting-edge decentralized distributed blockchains with 3D
+            metaverse client experiences.
+          </Text>
 
-              ml="auto"
-              p="5"
-              mt={{lg:"10", base:'72'}}
-              fontSize={{ lg: "3xl", md: "2xl", base: "sm" }}
-
-              align="left"
-              textColor="gray.600"
-            >
-              This Web 3.0 and Metaverse specialization focuses on developing
-              full-stack Web 3.0 and Metaverse experiences for the next
-              generation of the internet by specializing in building worlds that
-              merge the best of cutting-edge decentralized distributed
-              blockchains with 3D metaverse client experiences.
-            </Text>
-
-            <SimpleGrid
-              mt="-520"
-              spacing={0.2}
-              templateColumns="repeat(auto-fill, minmax(250px, 1fr))"
-            >
-              <Card>
-                <CardHeader>
-                  <Heading textColor="green.600" size="md">
-                    Quarter III W3-351: Developing Smart Contracts and
-                    Planet-Scale Web 3.0 Dapps
-                  </Heading>
-                </CardHeader>
-                <CardBody></CardBody>
-                <CardFooter>
-                  <Button
-                    textColor="blue.500"
-                    _hover={{
-                      textDecoration: "none",
-                      bg: useColorModeValue("blue.900", "blue.200"),
-                    }}
-                  >
-                    <Link color="teal.500" href="/BCQ3">
-                      Detailed Course Syllabus
-                    </Link>
-                  </Button>
-                </CardFooter>
-              </Card>
-              <Card>
-                <CardHeader>
-                  <Heading size="md" textColor="green.600">
-                    Quarter IV MV-361: Developing Planet-Scale Open Virtual and
-                    Augmented Metaverse Experiences
-                  </Heading>
-                </CardHeader>
-                <CardBody></CardBody>
-                <CardFooter>
-                  <Button
-                    textColor="blue.500"
-                    _hover={{
-                      textDecoration: "none",
-                      bg: useColorModeValue("blue.900", "blue.200"),
-                    }}
-                  >
-                   
-                    <Link color="teal.500" href="/BCQ4">
-                      Detailed Course Syllabus
-                    </Link>
-                  </Button>
-                </CardFooter>
-              </Card>
-            </SimpleGrid>
-          </Box>
+          <SimpleGrid
+            mt={{ lg: "-520", md: "-400" }}
+            spacing={0.2}
+            templateColumns="repeat(auto-fill, minmax(250px, 1fr))"
+          >
+            <Card>
+              <CardHeader>
+                <Heading textColor="green.600" size={{ lg: "md", base: "S" }}>
+                  Quarter III W3-351: Developing Smart Contracts and
+                  Planet-Scale Web 3.0 Dapps
+                </Heading>
+              </CardHeader>
+              <CardBody></CardBody>
+              <CardFooter>
+                <Button
+                  textColor="blue.500"
+                  _hover={{
+                    textDecoration: "none",
+                    bg: useColorModeValue("blue.900", "blue.200"),
+                  }}
+                >
+                  <Link color="teal.500" href="/BCQ3">
+                    Detailed Course Syllabus
+                  </Link>
+                </Button>
+              </CardFooter>
+            </Card>
+            <Card>
+              <CardHeader>
+                <Heading textColor="green.600" size={{ lg: "md", base: "S" }}>
+                  Quarter IV MV-361: Developing Planet-Scale Open Virtual and
+                  Augmented Metaverse Experiences
+                </Heading>
+              </CardHeader>
+              <CardBody></CardBody>
+              <CardFooter>
+                <Button
+                  textColor="blue.500"
+                  _hover={{
+                    textDecoration: "none",
+                    bg: useColorModeValue("blue.900", "blue.200"),
+                  }}
+                >
+                  <Link color="teal.500" href="/BCQ4">
+                    Detailed Course Syllabus
+                  </Link>
+                </Button>
+              </CardFooter>
+            </Card>
+          </SimpleGrid>
         </Box>
-      {/* //////////////////////////////////////////////////////////////////////////////////////////////////////////// */}
-      {/* <Box boxSize="3xl" p="5" mt="-20">
-          <Box as="div" mt="-48">
-            <Text
-              bg="linear-gradient(120deg, #155799, #159957)"
-              fontSize="3xl"
-              align="center"
-              textColor="gray.200"
-              borderRadius="20"
-            >
-              AI and Deep Learning Specialization
-            </Text>
-            <Text
-              boxSize="3xl"
-              ml="auto"
-              p="5"
-              mt="1"
-              fontSize="2xl"
-              align="left"
-              textColor="gray.600"
-            >
-              The AI and Deep Learning specialization focuses on building and
-              deploying intelligent APIs using OpenAI models and building custom
-              Deep Learning Tensorflow models.
-            </Text>
+      </Box>
+                      
 
-            <SimpleGrid
-              mt="-620"
-              spacing={0.2}
-              templateColumns="repeat(auto-fill, minmax(250px, 1fr))"
-            >
-              <Card>
-                <CardHeader>
-                  <Heading textColor="green.600" size="md">
-                    Quarter III W3-351: Developing Smart Contracts and
-                    Planet-Scale Web 3.0 Dapps
-                  </Heading>
-                </CardHeader>
-                <CardBody></CardBody>
-                <CardFooter>
-                  <Button
-                    textColor="blue.500"
-                    _hover={{
-                      textDecoration: "none",
-                      bg: useColorModeValue("blue.900", "blue.200"),
-                    }}
-                  >
-                    <Link color="teal.500" href="/AIQ3">
-                      Detailed Course Syllabus
-                    </Link>
-                  </Button>
-                </CardFooter>
-              </Card>
-              <Card>
-                <CardHeader>
-                  <Heading size="md" textColor="green.600">
-                    Quarter IV AI-361: Deep Learning and MLOps
-                  </Heading>
-                </CardHeader>
-                <CardBody></CardBody>
-                <CardFooter>
-                  <Button
-                    textColor="blue.500"
-                    _hover={{
-                      textDecoration: "none",
-                      bg: useColorModeValue("blue.900", "blue.200"),
-                    }}
-                  >
-                    <Link color="teal.500" href="/AIQ4">
-                      Detailed Course Syllabus
-                    </Link>
-                  </Button>
-                </CardFooter>
-              </Card>
-            </SimpleGrid>
-          </Box>
-        </Box> */}
+
+
       {/* //////////////////////////////////////////////////////////////////////////////////////////////////////////// */}
+      <Box
+        boxSize={{ lg: "3xl", md: "2xl" }}
+        ml={{ base: "0%" }}
+        p="5"
+      //  mt={{lg:'480',}}
+      >
+        <Box as="div" mt={{ lg: "-48", md: "-20",  }}>
+          <Text
+            bg="linear-gradient(120deg, #155799, #159957)"
+            fontSize={{ lg: "3xl", md: "2xl", base: "sm" }}
+            align="center"
+            textColor="gray.200"
+            borderRadius="20"
+          >
+            AI and Deep Learning Specialization
+          </Text>
+          <Text
+            boxSize={{ lg: "3xl", md: "2xl" ,base:"" }}
+            ml="auto"
+            p="5"
+            mt={{ lg: "10" }}
+            fontSize={{ lg: "3xl", md: "2xl", base: "sm" }}
+            align="left"
+            textColor="gray.600"
+          >
+            The AI and Deep Learning specialization focuses on building and
+            deploying intelligent APIs using OpenAI models and building custom
+            Deep Learning Tensorflow models.
+          </Text>
+          <SimpleGrid
+            // mt="-550"/
+            spacing={0.2}
+            templateColumns="repeat(auto-fill, minmax(250px, 1fr))"
+          >
+            <Card>
+              <CardHeader>
+                <Heading textColor="green.600" size="md">
+                  Quarter III W3-351: Developing Smart Contracts and
+                  Planet-Scale Web 3.0 Dapps
+                </Heading>
+              </CardHeader>
+              <CardBody></CardBody>
+              <CardFooter>
+                <Button
+                  textColor="blue.500"
+                  _hover={{
+                    textDecoration: "none",
+                    bg: useColorModeValue("blue.900", "blue.200"),
+                  }}
+                >
+                  <Link color="teal.500" href="/AIQ3">
+                    Detailed Course Syllabus
+                  </Link>
+                </Button>
+              </CardFooter>
+            </Card>
+            <Card>
+              <CardHeader>
+                <Heading size="md" textColor="green.600">
+                  Quarter IV AI-361: Deep Learning and MLOps
+                </Heading>
+              </CardHeader>
+              <CardBody></CardBody>
+              <CardFooter>
+                <Button
+                  textColor="blue.500"
+                  _hover={{
+                    textDecoration: "none",
+                    bg: useColorModeValue("blue.900", "blue.200"),
+                  }}
+                >
+                  <Link color="teal.500" href="/AIQ4">
+                    Detailed Course Syllabus
+                  </Link>
+                </Button>
+              </CardFooter>
+            </Card>
+          </SimpleGrid>
+        </Box>
+      </Box>
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+      {/* ////////////////////////////////////////////////////////////////////////////////////////////////////////////
       {/* <Box boxSize="3xl" p="5" mt="-20">
           <Box as="div" mt="-72">
             <Text
